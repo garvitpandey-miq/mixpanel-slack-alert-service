73f9daadc1b457ef77b383be1703198e8d9f1a05
Garvit Pandey <garvit.pandey@miqdigital.com>
2025-02-18 15:31:27 +0530

    Initial commit

---
+++ src/main/java/com/mediaiq/mixpanel_slack_alert_service/MixpanelSlackAlertServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mediaiq.mixpanel_slack_alert_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixpanelSlackAlertServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixpanelSlackAlertServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mediaiq/mixpanel_slack_alert_service/MixpanelSlackAlertServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mediaiq.mixpanel_slack_alert_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixpanelSlackAlertServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
